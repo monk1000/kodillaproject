814cc9530e5d747553af552d22d3705252172e9e
Tomasz Kacprzak <monkhtc@gmail.com>
2017-02-21 01:54:00 +0100

    gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+	grunt.initConfig({
+	  	watch: {
+    		scripts: {
+        		files: ['sass/*.sass'],
+        		tasks: ['sass'],
+        		options: {
+            		spawn: false,
+        		}
+    		} 
+		},
+
+	  	sass: {
+	    	options: {
+	    		sourceMap: true
+	    	},
+	    	dist: {
+	    		files: {
+	    			'css/main.css': 'sass/main.sass'
+	    		}
+	    	}
+	    },
+	    
+	    imagemin: {
+	    	dynamic: {
+	    		files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+		},
+
+		browserSync: {
+			bsFiles: {
+				src : 'assets/css/*.css'
+			},
+			options: {
+				watchTask: true,
+				server: {
+					baseDir: "./"
+        		}
+    		}
+		},
+	
+	});
+	// Load the plugins tasks 
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	// Default task(s).
+	grunt.registerTask('default', ['sass', 'imagemin','browserSync', 'watch']);
+};
